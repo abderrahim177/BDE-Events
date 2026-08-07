c891ecc3968a6a70449435f2fe660a0f8f244df7
abderrahim177 <abdorrahimakarchach@gmail.com>
2026-08-07 12:15:34 +0100

    creat stucture of react part

--- atlasventure-frontend/src/Api/axios.js
+++ atlasventure-frontend/src/Api/axios.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-
 const api = axios.create({
     baseURL: 'http://127.0.0.1:8000/api', 
     headers: {
@@ -7,7 +6,6 @@ const api = axios.create({
         'Accept': 'application/json',
     },
 });
-
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem('token');
     if (token) {
@@ -17,5 +15,4 @@ api.interceptors.request.use((config) => {
 }, (error) => {
     return Promise.reject(error);
 });
-
 export default api;
