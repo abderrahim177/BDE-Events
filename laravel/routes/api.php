6983faa6889960535aeca11bf74f49d2e35860eb
abderrahim177 <abdorrahimakarchach@gmail.com>
2026-08-13 21:44:55 +0100

    add rate limitting

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -9,33 +9,29 @@ use App\Http\Controllers\ReserverEventController;
 use App\Http\Controllers\TecketController;
 use App\Http\Middleware\RoleMiddleware;
 
-// Public Routes
-Route::post('/register', [AuthController::class, 'Register']);
-Route::post('/login', [AuthController::class, 'Login']);
+Route::post('/register', [AuthController::class, 'Register'])->middleware('throttle:5,1'); // 5 محاولات فالدقيقة
+Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:login');
 
-// Protected Routes (Require Token)
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware(['auth:sanctum', 'throttle:60,1'])->group(function () {
 
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
-
     Route::post('/logout', [AuthController::class, 'logout']);
 
-    // Student Only Routes
     Route::middleware(RoleMiddleware::class . ':student')->group(function () {
         Route::get('/students', [GetAllevenmentController::class, 'index']);
         Route::get('/ticket', [TecketController::class, 'store']);
         Route::post('/reservation/{id}', [ReserverEventController::class, 'store']);
-        Route::get('/totaleTiket' , [TecketController::class , "CountTiket"]);
+        Route::get('/totaleTiket', [TecketController::class, 'CountTiket']);
     });
-    // Admin Only Routes
+
     Route::middleware(RoleMiddleware::class . ':admin')->group(function () {
         Route::get('/admin', [GetAllevenmentController::class, 'DetailEvent']);
         Route::post('/admin/create', [CreateEvenmentController::class, 'Create']);
         Route::get('/eventManage', [CreateEvenmentController::class, 'index']);
         Route::get('/stats', [GetAllevenmentController::class, 'TotaleEvenment']);
-        Route::delete('/eventManage/{id}' , [CreateEvenmentController::class , 'destroy']);
+        Route::delete('/eventManage/{id}', [CreateEvenmentController::class, 'destroy']);
     });
 
 });
